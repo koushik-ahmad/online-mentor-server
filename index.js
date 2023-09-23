e0ea3f0602ace5970aba6d486a296ee672a2f7e3
Koushik Ahmad <koushikahmad123@gmail.com>
2023-09-23 15:36:21 +0600

    added mongodb

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,6 +8,33 @@ const port = process.env.PORT || 5000;
 // middle ware 
 app.use(cors());
 app.use(express.json());
+
+
+// user: onlineMentorDBUser
+// pass: YMSHam4CUoLpQqfY
+
+const uri = "mongodb+srv://onlineMentorDBUser:YMSHam4CUoLpQqfY@cluster0.mxcrgiz.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        const userCollection = client.db('onlineMentor').collection('users');
+
+
+
+        console.log("Connected to MongoDB");
+    } finally {
+
+    }
+}
+
+run().catch(err => console.log(err));
 
 
 
