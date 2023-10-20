ed881924374d9663bb73e7d5955bf0e8f3c54840
Koushik Ahmad <koushikahmad123@gmail.com>
2023-10-20 21:57:35 +0600

    added jwt token

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
@@ -11,6 +12,24 @@ app.use(cors());
 app.use(express.json());
 
 // const service = require('./service/service.json');
+
+// token function 
+function verifyJWT(req, res, next) {
+    const authHeaders = req.headers.authorization;
+    if (!authHeaders) {
+        res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeaders.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' });
+        }
+        else {
+            req.decoded = decoded;
+            next();
+        }
+    })
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mxcrgiz.mongodb.net/?retryWrites=true&w=majority`;
@@ -28,6 +47,13 @@ async function run() {
         const reviewCollection = client.db('onlineMentor').collection('review');
         const addServiceCollection = client.db('onlineMentor').collection('addService');
 
+        // jwt token
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1hr' });
+            res.send({ token });
+        });
+
         app.get('/service', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
@@ -35,11 +61,16 @@ async function run() {
             res.send(service);
         });
 
+        // pagination
         app.get('/services', async (req, res) => {
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
+            console.log(page, size);
             const query = {};
             const cursor = serviceCollection.find(query);
-            const service = await cursor.toArray();
-            res.send(service);
+            const services = await cursor.skip(page * size).limit(size).toArray();
+            const count = await serviceCollection.estimatedDocumentCount();
+            res.send({ count, services });
         });
 
         app.get('/service/:id', async (req, res) => {
@@ -55,15 +86,14 @@ async function run() {
             res.send(result);
         });
 
-        // app.get('/review', async (req, res) => {
-        //     const query = {};
-        //     const cursor = reviewCollection.find(query);
-        //     const review = await cursor.toArray();
-        //     res.send(review);
-        // });
-
         // review specific user query parameter
-        app.get('/review', async (req, res) => {
+        app.get('/review', verifyJWT, async (req, res) => {
+            const decoded = req.decoded;
+            console.log('inside review api', decoded);
+            if (decoded.email !== req.query.email) {
+                res.status(403).send({ message: 'unauthorized access' });
+            }
+
             let query = {};
 
             if (req.query.email) {
